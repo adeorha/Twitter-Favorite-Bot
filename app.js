f87c54ae566fe4aa20d2206c98aee908f2fd81d0
Aditya Deorha <aditya.deorha1@gmail.com>
2019-01-06 21:32:03 -0800

    Logging the entire tweet, 280 characters long, instead of 140 but using the full_text feature and adding to the search params.

--- app.js
+++ app.js
@@ -1,36 +1,33 @@
-const Twitter = require('twitter');
-const config = require('./config.js');
-const T = new Twitter(config);
+var Twitter = require('twitter');
+var config = require('./config.js');
+var T = new Twitter(config);
 
 // Set up your search parameters
-const params = {
-  q: '#nodejs',
-  count: 10,
-  result_type: 'recent',
-  lang: 'en'
-}
-
-// Initiate your search using the above paramaters
-T.get('search/tweets', params, (err, data, response) => {
-  // If there is no error, proceed
-  if(err){
-    return console.log(err);
+var params = {
+    q: '#nodejs',
+    count: 1,
+    result_type: 'recent',
+    lang: 'en',
+    tweet_mode: 'extended'
   }
 
-  // Loop through the returned tweets
-  const tweetsId = data.statuses
-    .map(tweet => ({ id: tweet.id_str }));
-
-  tweetsId.map(tweetId => {
-    T.post('favorites/create', tweetId, (err, response) => {
-      if(err){
-        return console.log(err[0].message);
+  T.get('search/tweets', params, function(err, data, response) {
+    if(!err){
+      for(let i = 0; i < data.statuses.length; i++){
+        let id = { id: data.statuses[i].id_str }
+        T.post('favorites/create', id, function(err, response){
+          if(err){
+            console.log(err[0].message);
+          }
+          else{
+            let username = response.user.screen_name;
+            let tweetId = response.id_str;
+            console.log('Favorited: ', `https://twitter.com/${username}/status/${tweetId}`)
+            console.log(data.statuses[i].retweeted_status.full_text)
+          }
+        });
       }
-
-      const username = response.user.screen_name;
-      const favoritedTweetId = response.id_str;
-      console.log(`Favorited: https://twitter.com/${username}/status/${favoritedTweetId}`);
-
-    });
-  });
-})
+    } else {
+      console.log(err);
+    }
+  })
